c5f675d213623c23ca157e239777655ebf711094
Pin Hao Lai <howardlai30425@gmail.com>
2021-06-28 11:09:13 +0800

    add sort algorithms

---
+++ sort_algorithms/Sort_an_Array912.py
@@ -0,0 +1,13 @@
+nums = [5,2,3,1]
+def bubbleSort(nums):
+    i=0
+    j=0
+    for i in range(len(nums)):
+        for j in range(len(nums)-1-i):
+            if nums[j]>=nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+    print(nums)
+    return nums
+bubbleSort(nums)
