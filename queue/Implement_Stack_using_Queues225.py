ae5217b5521cc485b735429650c79acca94a2a15
Pin Hao Lai <howardlai30425@gmail.com>
2021-06-30 15:12:42 +0800

    add queue

---
+++ queue/Implement_Stack_using_Queues225.py
@@ -0,0 +1,22 @@
+class MyStack(object):
+
+    def __init__(self):
+        self.array = []
+
+    def push(self, x):
+        self.array.append(x)
+
+    def pop(self):
+        popint = self.array[-1]
+        self.array.pop()
+        return popint 
+
+    def top(self):
+        return self.array[-1]
+
+    def empty(self):
+        if not self.array:
+            return True
+        else:
+            return False
+  
