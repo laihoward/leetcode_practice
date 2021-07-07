f2371944e44f334b2a37642d4366171d38fa5fc7
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-07 17:21:26 +0800

    Lowest_Common_Ancestor_of_a_BST235

---
+++ BST/Lowest_Common_Ancestor_of_a_BST235.py
@@ -0,0 +1,24 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if not root:
+            return None
+        if p.val>root.val and q.val>root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        elif p.val<root.val and q.val<root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        else:
+            return root
+
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
