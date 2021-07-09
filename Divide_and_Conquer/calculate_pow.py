8b3398caed024afd886f9bfe7f4ad5b8407a6e04
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-09 16:52:11 +0800

    use Divide and conquer

--- Divide_and_Conquer/calculate_pow.py
+++ Divide_and_Conquer/calculate_pow.py
@@ -1,7 +1,20 @@
-def power(x, y):
+def powerRecursive(x, y):
     if y == 0:
         return 1
     elif (int(y%2)==0):
-        return (power(x,int(y/2))*power(x,int(y/2)))
+        return (powerRecursive(x,int(y/2))*powerRecursive(x,int(y/2)))
     else:
-        return (x*power(x,int(y/2))*power(x,int(y/2)))
+        return (x*powerRecursive(x,int(y/2))*powerRecursive(x,int(y/2)))
+
+def powDivideConquer(x,y):
+    temp = 0
+    if y == 0:
+        return 1
+    temp = powDivideConquer(x,y//2)
+    if y%2 == 0:
+        return temp*temp
+    else:
+        return x*temp*temp
+
+
+print(powDivideConquer(2,3))
