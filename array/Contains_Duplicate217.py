d8db626f0d788946eeda41c6c6ebfe83cc988927
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-12 09:34:50 +0800

    Contains_Duplicate217

---
+++ array/Contains_Duplicate217.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution(object):
+    def containsDuplicate(self, nums):
+        count = Counter(nums)
+        for n in count:
+            print(count[n])
+            if count[n]>=2:
+                return True
+            else:
+                return False
+            
+        
+s=Solution()
+nums =[1,2,3,1]
+print(s.containsDuplicate(nums))
