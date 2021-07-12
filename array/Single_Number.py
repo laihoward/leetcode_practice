82d469fb58a5e72b852a3d3c50ce7e01abe2dcc4
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-12 09:54:16 +0800

    Single_Number

---
+++ array/Single_Number.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution(object):
+    def singleNumber(self, nums):
+        count = Counter(nums)
+        for n in count:
+            if count[n]==1:
+                return n
+
+
+    def singleNumber2(self, nums):
+        res=0
+        for num in nums:
+            res^=num
+        return res
+
+s=Solution()
+nums =[4,1,2,1,2]
+print(s.singleNumber(nums))
