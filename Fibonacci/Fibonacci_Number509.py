54e5029df7c822db3a446c8babfbde5bc4d75fdc
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-16 12:01:23 +0800

    Fibonacci use dynamic proframming

--- Fibonacci/Fibonacci_Number509.py
+++ Fibonacci/Fibonacci_Number509.py
@@ -1,8 +1,20 @@
 
-# O(2的n次方)
 class Solution(object):
+#use recursion  
+# O(2的n次方)
     def fib(self, n):
         if n<=1:
             return n
         else:
             return self.fib(n-1)+self.fib(n-2)
+# use Dynamic Programming
+#O(n)
+    def fib_dy(self, n):
+        fib_list=[0]*n
+        fib_list[0]=1
+        fib_list[1]=1
+        for i in range(2,n):
+            fib_list[i] = fib_list[i-1]+fib_list[i-2]
+        print(fib_list)
+s=Solution()
+s.fib_dy(7)
