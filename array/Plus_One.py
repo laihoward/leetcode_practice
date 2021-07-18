db4c2bdb7faa6fe1dc2bbd0d8eab0b4cc9c5117b
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-18 18:08:47 +0800

    Plus_One

---
+++ array/Plus_One.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, digits):
+        if not digits:
+            return
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+        digits.insert(0,1)
+        return digits
