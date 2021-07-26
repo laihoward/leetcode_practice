e0ace4ee031c0500fa5c27d6af3faa8944e828de
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-26 10:37:17 +0800

    Best_Time_to_Buy_and_Sell_Stock 2

--- Dynamic-Programming/Best_Time_to_Buy_and_Sell_Stock.py
+++ Dynamic-Programming/Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,17 +1,16 @@
 class Solution(object):
+    #Will Time Limit Exceeded
     def maxProfit(self, prices):
-        maxlist=[]
+        maxnum = 0
         for i in range(len(prices)):
-            maxnum = 0
+
             for j in range(i,len(prices)):
                 if prices[j]-prices[i]>maxnum and prices[j]-prices[i]>0:
                     maxnum = prices[j]-prices[i]
-            maxlist.append(maxnum)
-        print(max(maxlist))
-        return max(maxlist)
+        return maxnum
 
 #use Dynamic Programming
-    def maxProfit(self, prices):
+    def maxProfit2(self, prices):
         if not prices:
             return 0
         m = prices[0]
@@ -20,3 +19,7 @@ class Solution(object):
             priceslist[i] = max(priceslist[i-1],prices[i]-m)
             m=min(prices[i],m)
         return priceslist[-1]
+
+prices = [7,1,5,3,6,4]
+s=Solution()
+print(s.maxProfit(prices))
