765877ea12bf2696c250e67aa609007af90877c0
Pin Hao Lai <howardlai30425@gmail.com>
2021-07-27 09:01:22 +0800

    Min_Stack

---
+++ stack/Min_Stack.py
@@ -0,0 +1,26 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack =[]
+        
+
+    def push(self, val):
+        self.stack.append(val)
+        
+
+    def pop(self):
+        self.stack.pop()
+        
+
+    def top(self):
+        return self.stack[-1]
+        """
+        :rtype: int
+        """
+        
+
+    def getMin(self):
+        return min(self.stack)
+        """
+        :rtype: int
+        """
