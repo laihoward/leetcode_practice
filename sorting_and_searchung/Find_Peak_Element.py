a6fb9f7abb48d8c5e3076cb364d9f6522a5ab633
Pin Hao Lai <howardlai30425@gmail.com>
2021-08-05 12:35:41 +0800

    Find_Peak_Element

--- sorting_and_searchung/Find_Peak_Element.py
+++ sorting_and_searchung/Find_Peak_Element.py
@@ -10,22 +10,11 @@ class Solution(object):
                 start=middleR
             else:
                 end=middleL
-        return middleL
+        return start
 
-    def findPeakElement2(self, nums):
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid1 = (left + right) // 2
-            mid2 = mid1 + 1
-            if nums[mid1] < nums[mid2]:
-                left = mid2
-            else:
-                right = mid1
-        return left
 
         
 
 nums = [1,2,1,3,5,6,4]
 s=Solution()
 print(s.findPeakElement(nums))
-print(s.findPeakElement2(nums))
