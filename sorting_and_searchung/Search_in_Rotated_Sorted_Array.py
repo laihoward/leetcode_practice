57998a2875dee550ff6424104b04fe4c15f92f53
Pin Hao Lai <howardlai30425@gmail.com>
2021-08-09 12:00:20 +0800

    use binary search

--- sorting_and_searchung/Search_in_Rotated_Sorted_Array.py
+++ sorting_and_searchung/Search_in_Rotated_Sorted_Array.py
@@ -6,11 +6,22 @@ class Solution(object):
         return x
 
 
+    def search(self, nums, target):
+        left = 0
+        right=len(nums)-1
+        while left<=right:
+            middle =(left+right)//2
+            if nums[middle]==target:
+                return middle
+            elif nums[left]<=nums[middle]<target or target<nums[left]<=nums[middle] or nums[middle]<target<=nums[left]:
+                left=middle+1
+            else:
+                right=middle-1
+        return -1
 
 
 
-
-nums=[1]
-target  = 1
+nums=[4,5,6,7,0,1,2]
+target  = 0
 s=Solution()
 print(s.search(nums, target))
