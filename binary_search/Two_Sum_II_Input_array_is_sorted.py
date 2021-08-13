da7bb7ec600f207ec46817174833ff825cc55b67
Pin Hao Lai <howardlai30425@gmail.com>
2021-08-13 12:32:44 +0800

    Two Sum II - Input array is sorted

---
+++ binary_search/Two_Sum_II_Input_array_is_sorted.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        l = 0
+        r=len(numbers)-1
+        while l<=r:
+            res = numbers[l]+numbers[r]
+            if res==target:
+                return [l+1,r+1]
+            if res<target:
+                l+=1
+            else:
+                r-=1
+        
+
+
+
+
+
+
+numbers = [3,24,50,79,88,150,345]
+target = 200
+s=Solution()
+print(s.twoSum(numbers,target))
