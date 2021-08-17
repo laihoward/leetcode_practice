072570b47ef2c5ac4c747a3dfb2470d69cb21fa8
Pin Hao Lai <howardlai30425@gmail.com>
2021-08-17 13:40:27 +0800

    review selection sort

--- sort_algorithms/review_sort.py
+++ sort_algorithms/review_sort.py
@@ -1,10 +1,30 @@
 def bubble_sort(num):
     for i in range(len(num)):
-        for j in range(i,len(num)):
-            if num[i]>num[j]:
-                num[i],num[j]=num[j],num[i]
+        for j in range(len(num)-i-1):
+            if num[j]>num[j+1]:
+                num[j+1],num[j]=num[j],num[j+1]
     print(num)
     return num
+
+
+
+def selection_sort(num):
+    len_num=len(num)
+    
+    for i in range(len_num):
+        res = i
+        for j in range(i,len_num-1):
+            if num[res]>num[j+1]:
+                res = j+1
+        # print(num[res])
+        num[i],num[res]=num[res],num[i]
+    print(num)
+    return num
+
+
+
+
     
 array = [38,27,43,3,9,82,10]
-bubble_sort(array)
+# bubble_sort(array)
+selection_sort(array)
